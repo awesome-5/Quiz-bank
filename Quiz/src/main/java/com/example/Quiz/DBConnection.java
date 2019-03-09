cf05c735ea793205b34cbd2cf9b5e6e9321b373d
Nastassja <1092992@students.wits.ac.za>
2019-03-09 18:10:05 +0200

    Updated to connect to server

---
+++ Quiz/src/main/java/com/example/Quiz/DBConnection.java
@@ -0,0 +1,117 @@
+package com.example.Quiz;
+//
+//import java.beans.Statement;
+//import java.sql.Connection;
+//import java.util.Scanner;
+//import java.sql.*; 
+//import java.util.*; 
+//
+////public class DBConnection {
+////
+////}
+//
+//class DBConnection 
+//{ 
+//    public static void DBConnection() 
+//    { 
+//        //Creating the connection 
+//        String url = "jdbc:oracle:thin:@localhost:1521:xe"; 
+//        String user = "system"; 
+//        String pass = "12345"; 
+//  
+//        //Entering the data 
+//        Scanner k = new Scanner(System.in); 
+//        System.out.println("enter name"); 
+//        String name = k.next(); 
+//        System.out.println("enter roll no"); 
+//        int roll = k.nextInt(); 
+//        System.out.println("enter class"); 
+//        String cls =  k.next(); 
+//  
+//        //Inserting data using SQL query 
+//        String sql = "insert into student1 values('"+name+"',"+roll+",'"+cls+"')"; 
+//        Connection con=null; 
+//        try
+//        { 
+//            DriverManager.registerDriver(new oracle.jdbc.OracleDriver()); 
+//  
+//            //Reference to connection interface 
+//            con = DriverManager.getConnection(url,user,pass); 
+//  
+//            Statement st = (Statement) con.createStatement(); 
+//            int m = ((java.sql.Statement) st).executeUpdate(sql); 
+//            if (m == 1) 
+//                System.out.println("inserted successfully : "+sql); 
+//            else
+//                System.out.println("insertion failed"); 
+//            con.close(); 
+//        } 
+//        catch(Exception ex) 
+//        { 
+//            System.err.println(ex); 
+//        } 
+//    } 
+//}
+
+
+import java.sql.Statement;
+
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.JSchException;
+import com.jcraft.jsch.Session;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+
+public class DBConnection {
+
+	public String readDB() throws ClassNotFoundException, JSchException, SQLException {
+		
+		String output = "Failed to SQL";
+		int lport=5656;
+		String rhost="127.0.0.1";
+		String host="lamp.ms.wits.ac.za";
+		int rport=3306;
+		String dbUrl = "jdbc:mysql://"+rhost+":"+lport+"/d1268698";
+		String uName = "s1268698";
+		String uPass = "s1268698";
+		Connection con = null;
+		Session session =null;
+		
+		try {
+			
+			  JSch jsch = new JSch();
+	            session = jsch.getSession(uName, host, 22);
+	            session.setPassword(uPass);
+	            session.setConfig("StrictHostKeyChecking", "no");
+	            System.out.println("Establishing Connection...");
+	            session.connect();
+
+	            int assigned_port = session.setPortForwardingL(lport, rhost, rport);
+	            System.out.println("localhost:" + assigned_port + " -> " + rhost + ":" + rport);
+	    	
+	    	
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			con = DriverManager.getConnection(dbUrl, uName, uPass);
+			System.out.println ("Database connection established");
+			Statement statement = con.createStatement();
+
+			String sql = "SELECT * FROM User";
+		    ResultSet rs = statement.executeQuery(sql);
+			
+		    while (rs.next()) {
+		    	output =rs.getString("username");
+            }
+			
+
+		} catch (SQLException err) {
+			output = err.getMessage();
+		}
+		session.disconnect();
+		con.close();
+		return(output);
+	}
+}
--- Quiz/src/main/java/com/example/Quiz/MyUI.java
+++ Quiz/src/main/java/com/example/Quiz/MyUI.java
@@ -1,47 +1,77 @@
 package com.example.Quiz;
 
+import java.sql.SQLException;
+
 import javax.servlet.annotation.WebServlet;
 
+import com.jcraft.jsch.JSchException;
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
+import com.vaadin.ui.Alignment;
 import com.vaadin.ui.Button;
+import com.vaadin.ui.Component;
+import com.vaadin.ui.FormLayout;
+import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Label;
+import com.vaadin.ui.Notification;
+import com.vaadin.ui.Panel;
+import com.vaadin.ui.PasswordField;
 import com.vaadin.ui.TextField;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.themes.ValoTheme;
 
-/**
- * This UI is the application entry point. A UI may either represent a browser window 
- * (or tab) or some part of an HTML page where a Vaadin application is embedded.
- * <p>
- * The UI is initialized using {@link #init(VaadinRequest)}. This method is intended to be 
- * overridden to add component to the user interface and initialize non-component functionality.
- */
+
 @Theme("mytheme")
 public class MyUI extends UI {
+	final VerticalLayout layout = new VerticalLayout();
+	final TextField userName = new TextField("Username : ");
+	final PasswordField password = new PasswordField("Password : ", "");
 
-    @Override
-    protected void init(VaadinRequest vaadinRequest) {
-        final VerticalLayout layout = new VerticalLayout();
-        
-        final TextField name = new TextField();
-        name.setCaption("Type your name here:");
 
-        Button button = new Button("Click Me");
-        button.addClickListener(e -> {
-            layout.addComponent(new Label("Thanks " + name.getValue() 
-                    + ", it works!"));
-        });
-        
-        layout.addComponents(name, button);
-        
-        setContent(layout);
-    }
+	Button logIn = new Button("Log in");
+	
+	Button create = new Button("Sign Up");
+	Button forgot = new Button("Forgot Password");
 
-    @WebServlet(urlPatterns = "/*", name = "MyUIServlet", asyncSupported = true)
-    @VaadinServletConfiguration(ui = MyUI.class, productionMode = false)
-    public static class MyUIServlet extends VaadinServlet {
-    }
+	@Override
+	protected void init(VaadinRequest vaadinRequest) {
+
+		try {
+			DBConnection dbc = new DBConnection();
+			Notification.show(dbc.readDB());
+		} catch (ClassNotFoundException | JSchException | SQLException e1) {
+		}
+		logIn.setStyleName(ValoTheme.BUTTON_BORDERLESS);
+		setContent(layout);  
+		userName.setRequiredIndicatorVisible(isVisible());
+		password.setRequiredIndicatorVisible(isVisible());
+		final Panel panel = new Panel ();
+		layout.setSizeFull();
+		layout.addComponent(panel);
+		panel.setWidth(null);
+		layout.setComponentAlignment(panel,Alignment.MIDDLE_CENTER );		
+		final FormLayout formlayout = new FormLayout();
+		formlayout.setMargin(true);
+		formlayout.setStyleName("loginForm");
+		formlayout.addComponent(userName);
+		formlayout.addComponent(password);
+		formlayout.addComponent(logIn);
+		formlayout.addComponent(create);
+		formlayout.addComponent(forgot);
+		logIn.addClickListener(log -> {
+			formlayout.addComponent(new Label("Thanks " + userName.getValue() + password.getValue()
+			+ ", it works!"));
+		});
+		panel.setContent(formlayout);
+	}
+
+	@WebServlet(urlPatterns = "/*", name = "MyUIServlet", asyncSupported = true)
+	@VaadinServletConfiguration(ui = MyUI.class, productionMode = false)
+	public static class MyUIServlet extends VaadinServlet {
+	}
+
+
 }
