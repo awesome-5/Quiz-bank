c68e5211cb3811d320b3a5a12465220057dbb004
SeaniRomeo <47977399+SeaniRomeo@users.noreply.github.com>
2019-03-18 02:14:07 +0200

    Add files via upload

--- Quiz/src/main/java/com/example/Quiz/MyUI.java
+++ Quiz/src/main/java/com/example/Quiz/MyUI.java
@@ -9,16 +9,15 @@ import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.event.FieldEvents.FocusEvent;
 import com.vaadin.event.FieldEvents.FocusListener;
-import com.vaadin.navigator.Navigator;
 import com.vaadin.navigator.View;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
 import com.vaadin.ui.Alignment;
 import com.vaadin.ui.Button;
-import com.vaadin.ui.Component;
 import com.vaadin.ui.FormLayout;
 import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Label;
+import com.vaadin.ui.Layout;
 import com.vaadin.ui.Notification;
 import com.vaadin.ui.Panel;
 import com.vaadin.ui.PasswordField;
@@ -31,7 +30,9 @@ import com.vaadin.ui.themes.ValoTheme;
 @Theme("mytheme")
 public class MyUI extends UI implements View {
 	
-	//initialising components
+	
+
+	//Initializing components
 	final VerticalLayout layout = new VerticalLayout();
 	final TextField userName = new TextField("Username : ");
 	final PasswordField password = new PasswordField("Password : ", "");
@@ -53,8 +54,7 @@ public class MyUI extends UI implements View {
 		
 		setContent(layout);  	
 		layout.setSizeFull();
-		
-		final Panel panel = new Panel ();
+	    final Panel panel = new Panel ();
 		panel.setWidth(null);
 		layout.addComponent(panel);
 		layout.setComponentAlignment(panel,Alignment.MIDDLE_CENTER );		
@@ -71,6 +71,7 @@ public class MyUI extends UI implements View {
 				String s=dbc.readDB("SELECT * FROM User WHERE username='"+userName.getValue()+"' AND password='"+password.getValue()+"'");
 				if (s.equals("Failed to SQL"))
 				{
+					
 					label.setValue("Invalid Login");
 					userName.clear();
 					password.clear();
@@ -84,6 +85,11 @@ public class MyUI extends UI implements View {
 		
 		userName.addFocusListener(new FocusListener(){
 
+			/**
+			 * 
+			 */
+			private static final long serialVersionUID = 1L;
+
 			@Override
 			public void focus(FocusEvent event) {
 				label.setValue(null);
@@ -92,26 +98,162 @@ public class MyUI extends UI implements View {
 		});
 		password.addFocusListener(new FocusListener(){
 
+			/**
+			 * 
+			 */
+			private static final long serialVersionUID = 1L;
+
 			@Override
 			public void focus(FocusEvent event) {
 				label.setValue(null);
 			}
 			
 		});
-		
 		create.addClickListener(e -> {
-			Notification.show("Sign up Clicked");
+			
+		  	//Initializing components
+			final VerticalLayout layout = new VerticalLayout();
+			final TextField firstName = new TextField("First name : ");
+			final TextField lastName  =  new TextField("Last name : ");
+			final TextField email = new TextField("Email : ");
+//			final TextField chooseUsername = new TextField("Choose username : ");
+			final TextField userName = new TextField("Username : ");
+			final PasswordField password = new PasswordField("Password : ", "");
+//			final PasswordField confirmPassword = new PasswordField("Confirm password : ");
+			
+			
+			HorizontalLayout hl=new HorizontalLayout();
+			Button  signUp = new Button("Sign up");
+			Button back = new Button("Back");
+			Label label = new Label(null);
+
+			
+			//add elements to display and style them
+			signUp.setStyleName(ValoTheme.BUTTON_FRIENDLY);
+			back.setStyleName(ValoTheme.BUTTON_FRIENDLY);
+			
+			firstName.setRequiredIndicatorVisible(isVisible());
+			lastName.setRequiredIndicatorVisible(isVisible());
+			email.setRequiredIndicatorVisible(isVisible());
+			//chooseUsername.setRequiredIndicatorVisible(isVisible());
+			userName.setRequiredIndicatorVisible(isVisible());
+			password.setRequiredIndicatorVisible(isVisible());
+		//	confirmPassword.setRequiredIndicatorVisible(isVisible());
+		
+			
+			setContent(layout);  	
+			layout.setSizeFull();
+		    final Panel panel_signin = new Panel ();
+		    panel_signin.setWidth(null);
+			layout.addComponent(panel_signin);
+			layout.setComponentAlignment(panel_signin,Alignment.MIDDLE_CENTER );		
+			final FormLayout formalayout_ = new FormLayout();
+			formalayout_.setMargin(true);
+			formalayout_.setStyleName("Signup form");
+			hl.addComponents(signUp,back);
+			formalayout_.addComponents(firstName,lastName,email,userName, password, label, hl);
+			panel_signin.setContent(formalayout_);
+			
+			
+			signUp.addClickListener(log -> {
+				try {
+					DBConnection dbc = new DBConnection();
+					String s=dbc.readDB("SELECT * FROM User WHERE username='"+userName.getValue()+"' AND password='"+password.getValue()+"'");
+					if (s.length()> 1)
+					{
+						
+						label.setValue("Already register");
+						userName.clear();
+						password.clear();
+					}
+					else {
+						s=dbc.readDB("INSERT INTO User (firstname,lastname,email,username,password) VALUES ("+firstName.getValue()+","+lastName.getValue()+","+email.getValue()+","+userName.getValue()+","+password.getValue());
+						
+					
+							
+							label.setValue("Registration successful");
+				
+						
+					}
+				} catch (ClassNotFoundException | JSchException | SQLException e1) {
+				}
+			});
+			
+			back.addClickListener(log -> {
+				//Done straight back
+				 UI.getCurrent().getPage().reload();
+			});
+			
 		});
 		
 		forgot.addClickListener(e -> {
-			Notification.show("Forgot Password clicked");
+
+			//Initializing components
+			final VerticalLayout layout = new VerticalLayout();
+			final TextField email = new TextField("Email : ");
+			
+			HorizontalLayout hl=new HorizontalLayout();
+			Button  Submit = new Button("Submit");
+			Button back = new Button("back");
+			Label label = new Label(null);
+			
+			
+			//add elements to display and style them
+			Submit.setStyleName(ValoTheme.BUTTON_FRIENDLY);
+			back.setStyleName(ValoTheme.BUTTON_FRIENDLY);
+			email.setRequiredIndicatorVisible(isVisible());
+			
+			setContent(layout);  	
+			layout.setSizeFull();
+			final Panel panel_signin = new Panel ();
+			panel_signin.setWidth(null);
+			layout.addComponent(panel_signin);
+			layout.setComponentAlignment(panel_signin,Alignment.MIDDLE_CENTER );		
+			final FormLayout formalayout_ = new FormLayout();
+			formalayout_.setMargin(true);
+			formalayout_.setStyleName("Reset password");
+			hl.addComponents(Submit,back);
+			formalayout_.addComponents(email, label,hl);
+			panel_signin.setContent(formalayout_ );
+			
+			
+			Submit.addClickListener(log -> {
+				try {
+					DBConnection dbc = new DBConnection();
+					String s=dbc.readDB("SELECT * FROM User WHERE username='"+email.getValue()+"'"); 
+					if (s.length()> 1)
+					{
+						//TODO send link to the email to reset password 
+						label.setValue("Email sent, please check your email");
+						
+					}
+					else {
+						label.setValue("Email doesn't exist, please register");
+					}
+				} catch (ClassNotFoundException | JSchException | SQLException e1) {
+				}
+			});
+			
+			back.addClickListener(log -> {
+				 UI.getCurrent().getPage().reload();
+			});
+			
+			
+			
 		});
 	
 	}
+	
+
 
 	@WebServlet(urlPatterns = "/*", name = "MyUIServlet", asyncSupported = true)
 	@VaadinServletConfiguration(ui = MyUI.class, productionMode = false)
 	public static class MyUIServlet extends VaadinServlet {
+
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = 1L;
 	}
 
 
