40afed8f8f9f179647b15e8ffd19dfa16745b75b
Nikola Zatezalo <1268698@students.wits.ac.za>
2019-04-16 12:05:54 +0200

    Merged Post Sprint 2
    All code merged will be used as master branch for the 3rd sprint

---
+++ Quiz/src/main/java/com/example/Quiz_Bank/MyUI.java
@@ -0,0 +1,48 @@
+package com.example.Quiz_Bank;
+
+import javax.servlet.annotation.WebServlet;
+
+import com.vaadin.annotations.Theme;
+import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.navigator.Navigator;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.server.VaadinServlet;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.TextField;
+import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
+
+@Theme("mytheme")
+public class MyUI extends UI {
+
+	static Navigator navigator;
+    protected static final String MAINVIEW = "main";
+    protected static final String LOGINVIEW = "login";
+    protected static final String HOMEPAGE = "home";
+    protected static final String SIGNUP = "signup";
+    protected static final String FORGOTVIEW = "forgot password";
+ 
+    @Override
+    protected void init(VaadinRequest request) {
+        getPage().setTitle("Navigation Example");
+
+        // Create a navigator to control the views
+        navigator = new Navigator(this, this);
+
+        // Create and register the views
+        navigator.addView("", new LoginView());
+        navigator.addView(LOGINVIEW, new LoginView());
+      navigator.addView(MAINVIEW, new MainView());
+      navigator.addView(HOMEPAGE, new HomePage());
+      navigator.addView(SIGNUP, new SignupView());
+      navigator.addView(FORGOTVIEW, new ForgotPassView());
+      
+    }
+
+
+    @WebServlet(urlPatterns = "/*", name = "MyUIServlet", asyncSupported = true)
+    @VaadinServletConfiguration(ui = MyUI.class, productionMode = false)
+    public static class MyUIServlet extends VaadinServlet {
+    }
+}
---
+++ Quiz/src/main/java/com/example/Quiz_Bank/QuestionForm.java
@@ -0,0 +1,113 @@
+package com.example.Quiz_Bank;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import com.jcraft.jsch.JSchException;
+import com.vaadin.data.Binder;
+import com.vaadin.event.ShortcutAction.KeyCode;
+import com.vaadin.event.selection.SelectionEvent;
+import com.vaadin.event.selection.SelectionListener;
+import com.vaadin.event.selection.SingleSelectionEvent;
+import com.vaadin.event.selection.SingleSelectionListener;
+import com.vaadin.server.Page;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.DateField;
+import com.vaadin.ui.FormLayout;
+import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.NativeSelect;
+import com.vaadin.ui.TextArea;
+import com.vaadin.ui.TextField;
+import com.vaadin.ui.UI;
+import com.vaadin.ui.themes.ValoTheme;
+
+public class QuestionForm extends FormLayout {
+
+	private TextArea questionText = new TextArea("Question Text");
+	private TextArea questionAnswer = new TextArea("Question Answer");
+	private TextField marks = new TextField("Marks");
+	private NativeSelect<QuestionDifficulty> difficulty = new NativeSelect<>("Question Difficulty");
+	private TextField time=new TextField("Time");
+	private NativeSelect<Question>questions=new NativeSelect<>("Variant of");
+	private NativeSelect<QuestionType> type = new NativeSelect<>("Question Type");
+	private TextField line = new TextField("Lines");
+	private TextField options = new TextField("MCQ Options");
+	private Button save = new Button("Save");
+	public  Button back = new Button("Back");
+
+	private QuestionService service = QuestionService.getInstance();
+	private Question question;
+	private MainView myUI;
+	private Binder<Question> binder = new Binder<>(Question.class);
+
+	public QuestionForm(MainView mainView) {
+		this.myUI = mainView;
+		questionText.setRequiredIndicatorVisible(true);
+		questionText.setWidth("100%");
+		questionAnswer.setWidth("100%");
+		marks.setRequiredIndicatorVisible(true);
+		difficulty.setRequiredIndicatorVisible(true);
+		time.setRequiredIndicatorVisible(true);
+		type.setRequiredIndicatorVisible(true);
+		line.setVisible(false);
+		options.setVisible(false);
+
+		setSizeUndefined();
+		
+		HorizontalLayout buttons = new HorizontalLayout(save,back);
+		addComponents(questionText, marks, difficulty, time, questionAnswer, type, line, options, buttons);
+		setWidth("100%");
+
+		type.setItems(QuestionType.values());
+		type.addValueChangeListener( event-> {
+
+			if(type.getValue()==QuestionType.StandardQuestion) //if standard question is chosen
+			{
+				line.setVisible(true);
+				options.setVisible(false);
+			}
+			else 	//if mcq question is chosen
+			{
+				line.setVisible(false);   
+				options.setVisible(true);
+			}
+		});
+
+
+
+		difficulty.setItems(QuestionDifficulty.values());
+		save.setStyleName(ValoTheme.BUTTON_PRIMARY);
+		save.setClickShortcut(KeyCode.ENTER);
+
+		binder.bindInstanceFields(this);
+
+		save.addClickListener(e -> {
+			try {
+				this.save();
+			} catch (ClassNotFoundException | JSchException | SQLException e1) {
+				e1.printStackTrace();
+			}
+		});
+		back.addClickListener(log -> {
+		//Page.getCurrent().reload();
+			setVisible(false);
+			MainView.toolbar.setVisible(true);
+			MainView.grid.setVisible(true);
+		});
+
+	}
+
+	public void setQuestion(Question question) throws ClassNotFoundException, JSchException, SQLException {
+		this.question = question;
+		binder.setBean(question); 
+		setVisible(true);
+		questionText.selectAll();
+	}
+
+	private void save() throws ClassNotFoundException, JSchException, SQLException {
+		service.save(question);
+		DBConnection dbc = new DBConnection();
+		dbc.sendToDBQuestion(question);
+		MainView.updateList();	
+		setVisible(false);
+	}
+}
